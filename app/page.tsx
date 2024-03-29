889e4ab77139988a34b160ba30f267f17d94d124
Temuujin Natsagnyam <temuujin.nat@gmail.com>
2024-03-28 23:01:02 -0300

    intro section added

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,9 @@
+import Intro from "@/components/intro";
+
 export default function Home() {
-  return <main></main>;
+  return (
+    <main className="flex flex-col items-center p-4">
+      <Intro />
+    </main>
+  );
 }
