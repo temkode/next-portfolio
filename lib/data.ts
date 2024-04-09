da8f03e622a3552019e63ce7d60578d42cbcd75d
Temuujin Natsagnyam <temuujin.nat@gmail.com>
2024-04-09 17:14:11 +0100

    About me text update

--- lib/data.ts
+++ lib/data.ts
@@ -35,28 +35,28 @@ export const links = [
 
 export const experiencesData = [
   {
-    title: "Graduated bootcamp",
-    location: "Miami, FL",
+    title: "Graduated college",
+    location: "London, UK",
     description:
-      "I graduated after 6 months of studying. I immediately found a job as a front-end developer.",
+      "I studied Computer Science, Math, and Further Math at A-Levels.",
     icon: React.createElement(LuGraduationCap),
     date: "2019",
   },
   {
-    title: "Front-End Developer",
-    location: "Orlando, FL",
+    title: "Full Stack Developer Intern",
+    location: "Watford, UK",
     description:
-      "I worked as a front-end developer for 2 years in 1 job and 1 year in another job. I also upskilled to the full stack.",
+      "I worked as a full-stack developer for 1 year at Starleaf, a cloud video conference company.",
     icon: React.createElement(CgWorkAlt),
-    date: "2019 - 2021",
+    date: "2019 - 2020",
   },
   {
-    title: "Full-Stack Developer",
-    location: "Houston, TX",
+    title: "Software Developer Apprentice",
+    location: "London, UK",
     description:
-      "I'm now a full-stack developer working as a freelancer. My stack includes React, Next.js, TypeScript, Tailwind, Prisma and MongoDB. I'm open to full-time opportunities.",
+      "I worked 2 years while studying a software development course at Softwire. I worked with clients such as Channel 5, Royal Pharmaceutical Society, and Air Charter Service",
     icon: React.createElement(FaReact),
-    date: "2021 - present",
+    date: "2021 - 2023",
   },
 ] as const;
 
