8a10d37b5b87548f9bcccdff08003f47ce5ded6b
Temuujin Natsagnyam <75379172+temkode@users.noreply.github.com>
2024-04-15 02:49:35 +0100

    Update intro.tsx

--- components/intro.tsx
+++ components/intro.tsx
@@ -60,10 +60,9 @@ export default function Intro() {
         animate={{ opacity: 1, y: 0 }}
       >
         <span className="font-bold">Hello, I'm Temuujin.</span> I'm a{" "}
-        <span className="font-bold">full-stack developer</span> with{" "}
+        <span className="font-bold">full-stack web developer</span> with{" "}
         <span className="font-bold">3 years</span> of experience. I enjoy
-        building <span className="italic">sites & apps</span>. My focus is{" "}
-        <span className="underline">React (Next.js)</span>.
+        building <span className="italic">sites & apps</span>.
       </motion.h1>
 
       <motion.div
